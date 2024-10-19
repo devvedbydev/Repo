a665d6bebb5fa57d99a9b73386d091b6da4d14c9
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 23:21:42 +0100

    Update FreakHax.lua

--- FreakHax.lua
+++ FreakHax.lua
@@ -6,6 +6,7 @@ local dev = "freaky"
 if dev == "freaky" then
     warn("Freaks only!!! (doesnt support shit pc's)")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Loader.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/AwesomeSauce.lua"))()
 else
     print("dev is no longer freaky </3")
 end
