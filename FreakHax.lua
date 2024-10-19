9f2f50cf950f2d695861dc7c0f3f56c3a89b9e6c
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 23:24:53 +0100

    Update FreakHax.lua

--- FreakHax.lua
+++ FreakHax.lua
@@ -5,9 +5,9 @@ local dev = "freaky"
 
 if dev == "freaky" then
     warn("Freaks only!!! (doesnt support shit pc's)")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Loader.lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/AwesomeSauce.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Loader.lua"))() -- Requires AwesomeSauce
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/AwesomeSauce.lua"))() -- Loads Env 4 Loader
 else
-    print("dev is no longer freaky </3")
+    print("dev is no longer freaky </3") -- Gibberish
 end
 
