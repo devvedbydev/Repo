ea785dcbf7236ecdf98d654e2b72a6dd98f09883
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 22:53:09 +0100

    Create FreakHax.lua

---
+++ FreakHax.lua
@@ -0,0 +1,12 @@
+-- Obfuscation loader
+-- Credits: Dear Moonsec V3
+
+local dev = "freaky"
+
+if dev == "freaky" then
+    warn("Freaks only!!! (doesnt support shit pc's)")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Loader.lua"))()
+else
+    print("dev is no longer freaky </3")
+end
+
