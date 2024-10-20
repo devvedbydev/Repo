ed5c46eaeebabcbe5972d5501447119edac0c084
dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-20 01:17:57 +0100

    Update FreakHax.lua

--- FreakHax.lua
+++ FreakHax.lua
@@ -6,9 +6,9 @@ if dev == "freaky" then
     warn("Freaks only!!! (doesnt support shit pc's)")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/Loader.lua"))() -- Requires AwesomeSauce
     loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Repo/refs/heads/main/AwesomeSauce.lua"))() -- Loads Env 4 Loader
-	setfenv(1, getfenv(require))  -- Overriding require to load our stuff
+    setfenv(1, getfenv(require))  -- Overriding require to load our stuff
     getfenv().game = nil -- Removing old environment
-	-- New env is loaded with more unc, old is removed to improve performance
+    -- New env is loaded with more unc, old is removed to improve performance
 else
     print("dev is no longer freaky </3") -- Gibberish
 end
